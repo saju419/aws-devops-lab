7bfba0f421b254cf3b39bceebd9af5bad510a383
Saju <sajuadi79@gmail.com>
2026-09-26 08:02:30 +0000

    terraform 8081 port update

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,12 @@ resource "aws_security_group" "devops_lab_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
